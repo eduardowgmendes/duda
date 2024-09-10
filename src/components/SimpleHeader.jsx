12d070c3d451350ff966c9857d2fc4d9fecf6d64
eduardomendes <eduardowilliangm@gmail.com>
2024-09-10 01:11:42 -0300

    path resource test for github pages

--- src/components/SimpleHeader.jsx
+++ src/components/SimpleHeader.jsx
@@ -5,8 +5,6 @@ import Search from "antd/es/input/Search";
 import { useState } from "react";
 import Container from "./layout/Container";
 
-import dudaLogo from './assets/logos/duda-dark-logo.svg'
-
 const { Header } = Layout;
 
 export default function SimpleHeader() {
@@ -69,7 +67,7 @@ export default function SimpleHeader() {
                     <Col xs={{ span: 24 }} sm={{ span: 24 }} md={{ span: 24 }} lg={{ span: 4 }} xl={{ span: 4 }} xxl={{ span: 2 }}>
                         <Flex align="center" justify="center">
                             <a href="/">
-                                <Image src={dudaLogo} width='6rem' preview={false} alt="..."/>
+                                <Image src='../duda/src/assets/logos/duda-dark-logo.svg' width='6rem' preview={false} alt="..."/>
                             </a>
                         </Flex>
                     </Col>
