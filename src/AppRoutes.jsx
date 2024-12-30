0992eefb7fde7b9cfa29916a30df02b8d14be4e1
eduardomendes <eduardowilliangm@gmail.com>
2024-12-30 20:53:28 -0300

    changed BrowserRouter to HashRouter

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./pages/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Shop from "./pages/Shop";
 import NotFound from "./pages/NotFound";
@@ -8,7 +8,7 @@ import StoresPage from "./pages/StoresPage";
 
 const AppRoutes = () => {
     return (
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/shop/:offerId" element={<Shop />} />
@@ -17,7 +17,7 @@ const AppRoutes = () => {
                 <Route path="/about" element={<About />} />
                 <Route path="*" element={<NotFound title='Oops! Você está perdido?' description='Parece que a página que você está procurando não existe mais ou foi movida. Mas não se preocupe! Navegue pelo menu acima ou volte para a página inicial para continuar explorando nosso site.'/>} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
